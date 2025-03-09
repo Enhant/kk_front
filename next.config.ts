f8c58ead70d97712d9d2a8699b90614f0a9dda08
enhant <daniel.kuzin.sn@gmail.com>
2025-03-09 17:04:04 +0300

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  output: "export", // Включает статический экспорт
+  basePath: "/kk_frontend", // Укажи имя репозитория
+  assetPrefix: "/kk_frontend/",
+  images: {
+    unoptimized: true, // Отключает оптимизацию изображений (иначе они не будут работать на GitHub Pages)
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
